5d0b848107c8454f80f0267d9d168d9f904b94cd
Nawavit Chuchaiputtipong <nawavit.c@kbtg.tech>
2018-07-05 16:48:31 +0700

    RandomNumber

---
+++ src/main/java/Day4/Demo.java
@@ -0,0 +1,10 @@
+package Day4;
+
+import java.util.Random;
+
+public class Demo {
+
+    public int generateNumber(Random random){
+        return random.nextInt(10);
+    }
+}
---
+++ src/test/java/Day4/RandomNumberTest.java
@@ -0,0 +1,20 @@
+package Day4;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class RandomNumberTest {
+    @Test
+    public void TEST001_random_5() {
+        Demo demo = new Demo();
+        assertEquals(5, demo.generateNumber(new Random(){
+            public int nextInt(int bound){
+                return 5;
+            }
+        }));
+    }
+}
